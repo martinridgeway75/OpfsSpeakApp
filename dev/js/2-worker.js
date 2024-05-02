7657361c6f77bfa7a9380805b6870ed74992102d
martin <martinridgeway75@users.noreply.github.com>
2024-05-03 02:35:03 +0900

    ...

--- dev/js/2-worker.js
+++ dev/js/2-worker.js
@@ -1,19 +1,77 @@
 
-//from main thread:
-//  data = { obj: {}, fileName: "recordsIdx", subDir: { path: "records", obj: {}, fileUid: "123-456789-78987987" } //files in records and rubrics subDir.s
-//  or
-//  data = { obj: {}, fileName: "studentData" } //studentData, snippets, recordsIdx, rubricsIdx
-
 onmessage = (e) => {
     const data = e.data;
 
-    if (data?.subDir) {
-        writeFileInSubDir(data.subDir);
-    }
-    writeFile(data.obj, data.fileName);
+    //single file read
+    //readFile(fileName).then( (obj) => {
+        //postMessage(obj);
+    //});
+
+
+    //content writes
+    writeFile(data.obj, data.fileName).then( () => {
+        if (!data?.subDir) {
+            postMessage("done file");
+            return;
+        }
+        writeFileToSubDir(data.subDir).then( () => {
+            postMessage("done new or edited record + file");
+        });
+    });
 }
 
-async function writeFileInSubDir(subDir) {
+function prepFileContent(obj) {
+    const str = JSON.stringify(obj);
+    const textEncoder = new TextEncoder();
+
+    return textEncoder.encode(str);
+}
+function convertFileContent(dataView) {
+    const textDecoder = new TextDecoder();
+    const str = textDecoder.decode(dataView);
+
+    return JSON.parse(str);
+}
+
+async function deleteFile(fileName) {
+    const opfsRoot = await navigator.storage.getDirectory();
+    
+    opfsRoot.removeEntry(fileName);
+}
+
+
+async function readFile(fileName) {
+    const opfsRoot = await navigator.storage.getDirectory();
+    const idxFileHandle = await opfsRoot.getFileHandle(fileName);
+    const idxAccessHandle = await idxFileHandle.createSyncAccessHandle();
+    const size = idxAccessHandle.getSize();
+    const dataView = new DataView(new ArrayBuffer(size));
+
+    idxAccessHandle.read(dataView, {at: 0});
+    idxAccessHandle.close();
+
+    return convertFileContent(dataView);
+}
+
+async function readFilesFromSubDir(arrOfFiles, path) {
+    const opfsRoot = await navigator.storage.getDirectory();
+    const subDir = opfsRoot.getDirectoryHandle(path);
+    
+    //TODO: for fileUid in arrOfFiles
+
+    let recordFileHandle = await subDir.getFileHandle(fileUid);
+    let recordAccessHandle = await recordFileHandle.createSyncAccessHandle();
+    let size = recordAccessHandle.getSize();
+    let dataView = new DataView(new ArrayBuffer(size));
+
+    recordAccessHandle.read(dataView, {at: 0});
+    recordAccessHandle.close();
+
+    return convertFileContent(dataView);
+
+}
+
+async function writeFileToSubDir(subDir) {
 //  subDir: { path: "records", obj: {}, fileUid: "123-456789-78987987" }
 
     const content = prepFileContent(subDir.obj);
@@ -27,14 +85,6 @@ async function writeFileInSubDir(subDir) {
     recordAccessHandle.flush();
     recordAccessHandle.close();
 }
-
-function prepFileContent(obj) {
-    const str = JSON.stringify(obj);
-    const textEncoder = new TextEncoder();
-
-    return textEncoder.encode(str);
-}
-
 async function writeFile(obj, fileName) {
     const content = prepFileContent(obj);
     const opfsRoot = await navigator.storage.getDirectory();
--- dev/js/2.js
+++ dev/js/2.js
@@ -4,16 +4,23 @@
 //WORKER COMMS
 
 //@Main thread
-//NOTE: obj is always an object
-const myWorker = new Worker("js/2-worker.js");
-    const obj = {payload: "TEST 2 message"};
 
-    function wrkrPostMsg(obj) {
+//WRITES:
+//  data = { obj: {}, fileName: "recordsIdx", subDir: { path: "records", obj: {}, fileUid: "123-456789-78987987" } //files in records and rubrics subDir.s
+//  or
+//  data = { obj: {}, fileName: "studentData" } //studentData, snippets, recordsIdx, rubricsIdx
+
+
+
+    const myWorker = new Worker("js/2-worker.js");
+    const obj = { obj: {}, fileName: "studentData" };
+
+    function workerPostMsg(obj) {
         myWorker.postMessage(obj);
     }
 
-    myWorker.onmessage = function(e) {
+    myWorker.onmessage = (e) => {
         console.log(e.data);
     }
 
-    wrkrPostMsg(obj);
+    workerPostMsg(obj);
